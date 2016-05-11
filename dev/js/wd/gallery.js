0c2fd9024c617ca6789aaee6c0ca36692847d24f
Werner Dweight <wd@wernerdweight.com>
2016-05-11 22:35:58 +0200

    Gallery pt. 2

--- dev/js/wd/gallery.js
+++ dev/js/wd/gallery.js
@@ -6,17 +6,35 @@ function WerneoGallery(){
 
 	WerneoGallery.prototype.create = function(gallery){
 		var galleryWidth = gallery.offsetWidth;						/// total current width of the gallery container
-		var gutter = 0;												/// margin between items
 		var items = gallery.querySelectorAll('.gallery-item');		/// items of the gallery
 		var images = [];											/// array to hold images
-		var lowest = -1;											/// holds height of the lowest image from images
+		var lowest;													/// holds height of the lowest image from images
 		var rows = [];												/// array of rows (images that will end up displayed in the same row)
 		var rowIndex = 0;											/// index of rows
 		var i,j;													/// iterators
 
+		var parameters = {
+			'gutter': 0,
+			'maxItemsPerLine': 0,
+			'minHeight': -1,
+			'maxHeight': -1,
+		};
+
+		/// extend parameters
 		if(typeof gallery.dataset.gutter !== typeof undefined && gallery.dataset.gutter !== null){
-			gutter = parseInt(gallery.dataset.gutter);
+			parameters.gutter = parseInt(gallery.dataset.gutter);
 		}
+		if(typeof gallery.dataset.maxItemsPerLine !== typeof undefined && gallery.dataset.maxItemsPerLine !== null){
+			parameters.maxItemsPerLine = parseInt(gallery.dataset.maxItemsPerLine);
+		}
+		if(typeof gallery.dataset.minHeight !== typeof undefined && gallery.dataset.minHeight !== null){
+			parameters.minHeight = parseInt(gallery.dataset.minHeight);
+		}
+		if(typeof gallery.dataset.maxHeight !== typeof undefined && gallery.dataset.maxHeight !== null){
+			parameters.maxHeight = parseInt(gallery.dataset.maxHeight);
+		}
+
+		lowest = parameters.maxHeight;
 
 		if(items.length > 0){
 			for (i = 0; i < items.length; i++) {
@@ -25,6 +43,10 @@ function WerneoGallery(){
 					lowest = images[i].naturalHeight;
 				}
 			}
+		}
+
+		if(lowest < parameters.minHeight){
+			lowest = parameters.minHeight;
 		}
 
 		if(items.length > 0){
@@ -44,7 +66,7 @@ function WerneoGallery(){
 				/// save row data
 				rows[rowIndex].contentWidth += width;
 				rows[rowIndex].items.push(i);
-				if(rows[rowIndex].contentWidth > galleryWidth){
+				if(rows[rowIndex].contentWidth > galleryWidth || (parameters.maxItemsPerLine > 0 && parameters.maxItemsPerLine <= rows[rowIndex].items.length)){
 					rowIndex++;
 				}
 			}
@@ -53,23 +75,19 @@ function WerneoGallery(){
 		if(rows.length > 0){
 			for (i = 0; i < rows.length; i++) {
 				/// calculate ratio to shrink items by
-				var scaleRatio = galleryWidth / (rows[i].contentWidth + ((rows[i].items.length - 1) * gutter));
-				console.log('r' + i + ': ' + (rows[i].contentWidth - galleryWidth));
+				var scaleRatio = galleryWidth / (rows[i].contentWidth + ((rows[i].items.length - 1) * parameters.gutter));
 				if(rows[i].items.length > 0){
 					var counter = 0;
 					for (j = 0; j < rows[i].items.length; j++) {
 						/// calculate scaled dimensions
 						var scaledWidth = items[rows[i].items[j]].offsetWidth * scaleRatio;
 						var scaledHeight = items[rows[i].items[j]].offsetHeight * scaleRatio;
-						console.log('w: ' + items[rows[i].items[j]].offsetWidth + ' -> ' + scaledWidth + 'px');
-						//console.log('h: ' + items[rows[i].items[j]].offsetHeight + ' -> ' + scaledHeight + 'px');
 						counter += scaledWidth;
 						/// set new dimensions
 						items[rows[i].items[j]].style.height = scaledHeight + 'px';
 						items[rows[i].items[j]].style.width = scaledWidth + 'px';
-						items[rows[i].items[j]].style.marginBottom = gutter + 'px';
+						items[rows[i].items[j]].style.marginBottom = parameters.gutter + 'px';
 					}
-					console.log(counter);
 				}
 			}
 		}
