897c308e7a3de813e8595d713769470aad9ff116
Werner Dweight <wd@wernerdweight.com>
2017-05-08 15:45:43 +0200

    Added timeout parameter for flash messages plugin

--- dev/js/wd/plugins/flashMessages.js
+++ dev/js/wd/plugins/flashMessages.js
@@ -2,61 +2,66 @@ import WerneoPlugin from "../core/plugin.js";
 
 class WerneoFlashMessages extends WerneoPlugin {
 	
-	constructor(){
+	constructor() {
 		super();
 		this.queue = null;
 		this.id = 1;
 	}
 
-	dismiss(id){
-		var _this = this;
+	dismiss(id) {
+		let _this = this;
 
-		_this.queue.querySelector('#flash-message-' + id).classList.remove('active');
+		_this.queue.querySelector(`#flash-message-${id}`).classList.remove('active');
 	}
 
-	show(id){
-		var _this = this;
+	show(id) {
+		let _this = this;
 
-		_this.queue.querySelector('#flash-message-' + id).classList.add('active');
+		_this.queue.querySelector(`#flash-message-${id}`).classList.add('active');
 		/// enable listener to dismiss message
-        _this.queue.querySelector('#flash-message-' + id + '-close').addEventListener('click',function(){
+		_this.queue.querySelector(`#flash-message-${id}-close`).addEventListener('click', () => {
 			_this.dismiss(id);
-        });
+		});
 	}
 
-	getMessageHtml(content,type,id){
-		var html = '<div class="' + type + ' flash-message" id="flash-message-' + id + '">';
-		html += content;
-		html += '<span class="close" id="flash-message-' + id + '-close">&times;</span>';
-		html += '</div>';
-
-		return html;
+	getMessage(content, type, id) {
+		let node = document.createElement('div');
+		node.classList.add(type);
+		node.classList.add(`flash-message`);
+		node.id = `flash-message-${id}`;
+		node.innerHTML = `<span class="close" id="flash-message-${id}-close">&times;</span>${content}`;
+		return node;
 	}
 
-	createMessage(content,type){
-		var _this = this;
-		var id = _this.id++;
-        
-        /// add flash message into the queue
-        _this.queue.innerHTML = _this.getMessageHtml(content,type,id) + _this.queue.innerHTML;
-        /// activate message
-        setTimeout(function(){
+	createMessage(content, type, timeout = 5000) {
+		let _this = this;
+		let id = _this.id++;
+		let delay = 100;
+		
+		/// add flash message into the queue
+		_this.queue.insertBefore(_this.getMessage(content, type, id), _this.queue.firstChild);
+
+		/// activate message
+		setTimeout(() => {
 			_this.show(id);
-        },100);
-        /// automatically dismiss message after 5 s
-        setTimeout(function(){
-			_this.dismiss(id);
-        },5100);
+		}, delay);
+
+		/// automatically dismiss message after [timeout] ms
+		if(parseInt(timeout) > 0) {
+			setTimeout(() => {
+				_this.dismiss(id);
+			}, parseInt(timeout) + delay);
+		}
 	}
 
-	invoke(){
-		var _this = this;
+	invoke() {
+		let _this = this;
 
 		/// append queue if not present
-        if(null === document.getElementById('flash-message-queue')){
-            document.querySelector('body').innerHTML += '<div id="flash-message-queue" class="flash-message-queue"></div>';
-        }
-        _this.queue = document.getElementById('flash-message-queue');
+		if(null === document.getElementById('flash-message-queue')) {
+			document.querySelector('body').innerHTML += `<div id="flash-message-queue" class="flash-message-queue"></div>`;
+		}
+		_this.queue = document.getElementById('flash-message-queue');
 	}
 
 }
